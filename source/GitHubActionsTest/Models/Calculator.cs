fe29f5326dc28036c4fe011c0c32f77be7b12c89
minfia <minfialab@gmail.com>
2021-11-08 15:15:56 +0900

    Update: add calculator test

--- source/GitHubActionsTest/Models/Calculator.cs
+++ source/GitHubActionsTest/Models/Calculator.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GitHubActionsTest.Models
 {
-    class Calculator
+    public class Calculator
     {
         public int Add(int a, int b)
         {
--- source/UnitTest/UnitTest1.cs
+++ source/UnitTest/UnitTest1.cs
@@ -1,18 +1,28 @@
-﻿using System;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace UnitTest
 {
+    using GitHubActionsTest.Models;
     public class UnitTest1
     {
+        Calculator calculator;
         [SetUp]
         public void SetUp()
         {
+            calculator = new Calculator();
         }
 
-        [Test]
-        public void TestMethod1()
+        [TestCase(0, 0, 0)]
+        [TestCase(1, 3, 4)]
+        [TestCase(3, 43, 46)]
+        [TestCase(53, 4, 57)]
+        [TestCase(6432, 322, 6754)]
+        [TestCase(32, 5454, 5486)]
+        [TestCase(95, 930, 1025)]
+        public void AddTest(int a, int b, int res)
         {
+            int val = calculator.Add(a, b);
+            Assert.AreEqual(res, val);
         }
     }
 }
