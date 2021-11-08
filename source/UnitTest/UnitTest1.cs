8a693880c63b8d1e4e0d4e9b7376d943a83c8266
minfia <minfialab@gmail.com>
2021-11-08 14:08:23 +0900

    Update: change test tool MSTest to NUnit

--- source/UnitTest/UnitTest1.cs
+++ source/UnitTest/UnitTest1.cs
@@ -1,12 +1,16 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 
 namespace UnitTest
 {
-    [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [Test]
         public void TestMethod1()
         {
         }
